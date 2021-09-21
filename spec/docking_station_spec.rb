06bb23535a58dc7d7bd2fc0956b48ead00b57a5d
Tom <t.p.kellett@gmail.com>
2021-09-21 16:20:09 +0100

    Feature tested the user stories
    Co-authored-by: Conor <cwdbutler@gmail.com>

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,10 @@ describe DockingStation do
   it "responds to release_bike method" do
     expect(docking_station).to respond_to(:release_bike)
   end
+  
+  it "user gets a bike and the bike works" do
+    bike = docking_station.release_bike
+    expect(bike).to be_a Bike
+    expect(bike.working?).to eq true
+  end
 end
